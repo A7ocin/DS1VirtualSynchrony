7e0e747bc11843467c013688d2eb03ff750cc83a
A7ocin <mr_nico@hotmail.it>
2018-08-18 00:12:32 +0200

    Started modeling unstable/stable messages. TODO: implement flush mechanism and make sure everything works correctly

--- DS1VirtualSynchrony/src/main/java/it/unitn/ds1/Actors/GenericActor.java
+++ DS1VirtualSynchrony/src/main/java/it/unitn/ds1/Actors/GenericActor.java
@@ -25,6 +25,7 @@ import java.sql.Timestamp;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.Random;
+import java.util.HashSet;
 import java.time.*;
 
 /**
@@ -45,6 +46,8 @@ public abstract class GenericActor extends AbstractActor{
     private int Td = 1000;                  // Time threshold for message exchange
     public int Ttimeout = 5000;            // Timeout for heartbeat receival
     public ActorRef manager;
+    private HashMap<Integer, Message> unstableMessages = new HashMap<Integer, Message>();
+    private HashSet<String> delivered = new HashSet<String>();
 
     /**
      * Generic Actor constructor.
@@ -95,6 +98,22 @@ public abstract class GenericActor extends AbstractActor{
     public void sendUnstableMessages(){
 
         System.out.format("[%d] Sending unstable messages\n", myId);
+        Iterator<HashMap.Entry<Integer, Message>> itUnstable = unstableMessages.entrySet().iterator();
+        while(itUnstable.hasNext()){
+            HashMap.Entry<Integer, Message> um = itUnstable.next();
+            Iterator<HashMap.Entry<Integer, ActorRef>> it = v.participants.entrySet().iterator();
+            while (it.hasNext()) {
+                HashMap.Entry<Integer, ActorRef> participant = it.next();
+                participant.getValue().tell(um.getValue(), getSelf());
+                System.out.format("[%d] Sending unstable message %s to partecipant %d\n", myId, um.getValue().body, participant.getKey());
+                try{
+                    networkDelay();
+                }
+                catch(Exception e){
+                    System.out.println("SLEEP ERROR");
+                }
+            }
+        }
 
     }
 
@@ -139,16 +158,16 @@ public abstract class GenericActor extends AbstractActor{
         }
     }
 
-    public void sendChatMessage(){
-        Date date= new Date();
+    public void sendChatMessage() {
+        Date date = new Date();
         long time = date.getTime();
-        String ts = new Timestamp(time).toString();
+        String ts = "[" + this.myId + "] " + new Timestamp(time).toString();
 
         Message m = new Message(myId, ts);
         Iterator<HashMap.Entry<Integer, ActorRef>> it = v.participants.entrySet().iterator();
         while (it.hasNext()) {
             HashMap.Entry<Integer, ActorRef> participant = it.next();
-            if(participant.getKey() == this.myId){
+            if (participant.getKey() == this.myId) {
                 continue;
             }
             participant.getValue().tell(m, getSelf());
@@ -165,51 +184,6 @@ public abstract class GenericActor extends AbstractActor{
                     }
                 }, this.getContext().getSystem().dispatcher());
     }
-
-
-//    public void sendHeartbeat(){
-//
-//        Heartbeat h = new Heartbeat(myId);
-//        ActorRef manager = v.participants.get(0);
-//        manager.tell(h, getSelf());
-//        networkDelay();
-//
-//        Heartbeat h = new Heartbeat(myId);
-//        Iterator<HashMap.Entry<Integer, ActorRef>> it = v.participants.entrySet().iterator();
-//        while (it.hasNext()) {
-//            HashMap.Entry<Integer, ActorRef> participant = it.next();
-//            if(participant.getKey() == this.myId){
-//                continue;
-//            }
-//            participant.getValue().tell(h, getSelf());
-//            //System.out.format("[%d] Sent heartbeat to %d\n", myId, participant.getKey());
-//
-//            // CRASH DETECTION
-//            try {
-//                Instant previous = heartbeats.get(participant.getKey());
-//                long delta = Duration.between(previous, Instant.now()).toMillis();
-//                if (delta > Ttimeout) {
-//                    System.out.format("[%d] Process %d CRASHED!!! %d\n", myId, participant.getKey(), delta);
-//                    ActorRef manager = v.participants.get(0);
-//                    CrashDetected crash = new CrashDetected(this.myId, participant.getKey());
-//                    manager.tell(crash, getSelf());
-//                }
-//            }
-//            catch(Exception e){
-//                // TODO
-//            }
-//
-//            networkDelay();
-//        }
-//
-//        this.getContext().getSystem().scheduler().scheduleOnce(java.time.Duration.ofMillis(1000),
-//                new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        sendHeartbeat();
-//                    }
-//                }, this.getContext().getSystem().dispatcher());
-//    }
 
     public void onChangeView(ChangeView request){
         if(isCrashed() || !canInstallView(request.v)){
@@ -236,7 +210,13 @@ public abstract class GenericActor extends AbstractActor{
         if(isCrashed()){
             return;
         }
-        System.out.format("[%d] Received message %s from %d\n", myId, message.body, message.senderId);
+
+        // Check if duplicate
+        if(!delivered.contains(message.body)){
+            unstableMessages.put(message.senderId, message);
+            delivered.add(message.body);
+            System.out.format("[%d] Received message %s from %d\n", myId, message.body, message.senderId);
+        }
 
     }
 
